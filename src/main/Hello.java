3e7b00b0f0fb5c1d5b3e1acb12663b7390417308
34011-12-11 <34011-12-11@STA6300181.exchange.ad.afpanet>
2019-05-23 14:27:59 +0200

    add voiture Class

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-
+       Voiture v = new Voiture("toyota","grise");
+       System.out.println(v);
 	}
 
 }
