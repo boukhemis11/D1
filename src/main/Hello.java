82d2d1a1bc9c50eef562b7831c139b70c9b27330
34011-12-11 <34011-12-11@STA6300181.exchange.ad.afpanet>
2019-05-23 14:53:08 +0200

    delete world

--- src/main/Hello.java
+++ src/main/Hello.java
@@ -3,7 +3,7 @@ package main;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+	
        Voiture v = new Voiture("toyota","grise");
        System.out.println(v);
 	}
